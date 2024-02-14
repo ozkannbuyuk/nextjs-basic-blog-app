fe9d6c36a1932b01d01cf6de12c886e500375231
ozkanbuyuk <ozkanbuyuk@icloud.com>
2024-02-14 15:12:49 +0300

    Next.js - Basic Blog App

---
+++ app/head.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Head() {
+  return (
+    <>
+      <title>Next.js - Basic Blog App</title>
+      <meta name="description" content="Next.js - Basic Blog App" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
+
+export default Head;
---
+++ components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h4>Next.js - Basic Blog App 🎉</h4>
+    </footer>
+  );
+}
+
+export default Footer;
